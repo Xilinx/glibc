c95ea94d72ccb1c0427f2f58435303291213748a
David Holsgrove <david.holsgrove@petalogix.com>
2011-02-10 11:17:59 +1000

    Applying patch: 'patches/020-microblaze-little-endian.patch'

--- sysdeps/microblaze/bits/endian.h
+++ sysdeps/microblaze/bits/endian.h
@@ -4,4 +4,8 @@
 # error "Never use <bits/endian.h> directly; include <endian.h> instead."
 #endif
 
+#ifdef _BIG_ENDIAN
 #define __BYTE_ORDER __BIG_ENDIAN
+#else
+#define __BYTE_ORDER __LITTLE_ENDIAN
+#endif
