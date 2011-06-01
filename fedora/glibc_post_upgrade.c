6babb2b725aa61b117bb68440a409b5f05db71f6
Andreas Schwab <schwab@redhat.com>
2011-06-01 17:12:20 +0200

    Don't build tzdata-update and build-locale-archive statically

--- fedora/glibc_post_upgrade.c
+++ fedora/glibc_post_upgrade.c
@@ -1,6 +1,3 @@
-#if defined __sparc__ && defined __arch64__
-register void *__thread_self __asm ("g7");
-#endif
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
@@ -194,60 +191,6 @@ main (void)
 
   _exit(0);
 }
-
-#ifndef NO_SIZE_OPTIMIZATION
-int __libc_multiple_threads __attribute__((nocommon));
-int __libc_enable_asynccancel (void) { return 0; }
-void __libc_disable_asynccancel (int x) { }
-void __libc_csu_init (void) { }
-void __libc_csu_fini (void) { }
-pid_t __fork (void) { return -1; }
-char thr_buf[65536];
-
-# ifndef __powerpc__
-int
-__libc_start_main (int (*main) (void), int argc, char **argv,
-		   void (*init) (void), void (*fini) (void),
-		   void (*rtld_fini) (void), void * stack_end)
-# else
-struct startup_info
-{
-  void *sda_base;
-  int (*main) (int, char **, char **, void *);
-  int (*init) (int, char **, char **, void *);
-  void (*fini) (void);
-};
-
-int
-__libc_start_main (int argc, char **ubp_av, char **ubp_ev,
-		   void *auxvec, void (*rtld_fini) (void),
-		   struct startup_info *stinfo,
-		   char **stack_on_entry)
-# endif
-{
-#if defined __ia64__ || defined __powerpc64__
-  register void *r13 __asm ("r13") = thr_buf + 32768;
-  __asm ("" : : "r" (r13));
-#elif defined __sparc__
-  register void *g6 __asm ("g6") = thr_buf + 32768;
-# ifdef __arch64__
-  __thread_self = thr_buf + 32768;
-# else
-  register void *__thread_self __asm ("g7") = thr_buf + 32768;
-# endif
-  __asm ("" : : "r" (g6), "r" (__thread_self));
-#elif defined __s390__ && !defined __s390x__
-  __asm ("sar %%a0,%0" : : "d" (thr_buf + 32768));
-#elif defined __s390x__
-  __asm ("sar %%a1,%0; srlg 0,%0,32; sar %%a0,0" : : "d" (thr_buf + 32768) : "0");
-#elif defined __powerpc__ && !defined __powerpc64__
-  register void *r2 __asm ("r2") = thr_buf + 32768;
-  __asm ("" : : "r" (r2));
-#endif
-  main();
-  return 0;
-}
-#endif
 
 void
 vexec (int failcode, char *const path[])
