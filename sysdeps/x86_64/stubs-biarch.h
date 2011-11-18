37659c5e9e2a0983c044cd615d93d5639240393e
H.J. Lu <hjl.tools@gmail.com>
2011-11-18 15:58:11 -0800

    Initial x32 support.

---
+++ sysdeps/x86_64/stubs-biarch.h
@@ -0,0 +1,8 @@
+/* This file selects the right generated file of `__stub_FUNCTION' macros
+   based on the architecture being compiled for.  */
+
+#ifdef __x86_64__
+# include <gnu/stubs-64.h>
+#else
+# include <gnu/stubs-32.h>
+#endif
