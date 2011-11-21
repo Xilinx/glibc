6ee6cabdb84596c13d929069c9f453e7e31186e1
H.J. Lu <hjl.tools@gmail.com>
2011-11-21 14:49:13 -0800

    Add x32 support to mathdef.h/setjmp.h/fenv.h.

--- sysdeps/x86_64/fpu/bits/fenv.h
+++ sysdeps/x86_64/fpu/bits/fenv.h
@@ -20,9 +20,6 @@
 # error "Never use <bits/fenv.h> directly; include <fenv.h> instead."
 #endif
 
-#include <bits/wordsize.h>
-
-
 /* Define bits representing the exception.  We use the bit positions
    of the appropriate bits in the FPU control word.  */
 enum
@@ -82,7 +79,7 @@ typedef struct
     unsigned int __data_offset;
     unsigned short int __data_selector;
     unsigned short int __unused5;
-#if __WORDSIZE == 64
+#if __x86_64__
     unsigned int __mxcsr;
 #endif
   }
