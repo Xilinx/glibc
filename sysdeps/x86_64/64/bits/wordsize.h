446d36fb12959c87f97ef2d2322d1372be0c5005
H.J. Lu <hjl.tools@gmail.com>
2012-03-16 14:07:25 -0700

    Support GCC older than GCC 3.3
    2012-03-16  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* sysdeps/x86_64/64/bits/wordsize.h (__GNUC_PREREQ): New macro
    	if not defined.
    	(__WORDSIZE): Defined to 64 for GCC older than GCC 3.3 if
    	__x86_64__ is defined.
    	(__WORDSIZE_COMPAT32): Restored.
    	* sysdeps/x86_64/x32/bits/wordsize.h: Likewise.

--- sysdeps/x86_64/64/bits/wordsize.h
+++ sysdeps/x86_64/64/bits/wordsize.h
@@ -1,7 +1,18 @@
-/* Determine the wordsize from the preprocessor defines.  */
+/* Determine the wordsize from the preprocessor defines.  __LP64__ is
+   defined for 64bit since GCC 3.3.  For GCC older than GCC 3.3, if
+   __x86_64__ is defined, it must be 64bit.  */
+#ifndef __GNUC_PREREQ
+# if defined __GNUC__ && defined __GNUC_MINOR__
+#  define __GNUC_PREREQ(maj, min) \
+	((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
+# else
+#  define __GNUC_PREREQ(maj, min) 0
+# endif
+#endif
 
-#if defined __LP64__
+#if defined __x86_64__ && (__LP64__ || !__GNUC_PREREQ (3, 3))
 # define __WORDSIZE	64
+# define __WORDSIZE_COMPAT32	1
 #else
 # define __WORDSIZE	32
 #endif
