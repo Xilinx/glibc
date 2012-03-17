6861fb0f6a7f641dfb7db21f237469bd7adea598
H.J. Lu <hjl.tools@gmail.com>
2012-03-17 09:25:28 -0700

    Check __GNUC__ directly for __WORDSIZE
    2012-03-17  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* sysdeps/x86_64/64/bits/wordsize.h (__GNUC_PREREQ): Removed.
    	(__WORDSIZE): Check __GNUC__ directly.
    	* sysdeps/x86_64/x32/bits/wordsize.h: Likewise.

--- sysdeps/x86_64/x32/bits/wordsize.h
+++ sysdeps/x86_64/x32/bits/wordsize.h
@@ -1,16 +1,9 @@
 /* Determine the wordsize from the preprocessor defines.  __LP64__ is
-   defined for 64bit since GCC 3.3.  For GCC older than GCC 3.3, if
+   defined for 64bit since GCC 3.3.  For GCC older than GCC 4.0, if
    __x86_64__ is defined, it must be 64bit.  */
-#ifndef __GNUC_PREREQ
-# if defined __GNUC__ && defined __GNUC_MINOR__
-#  define __GNUC_PREREQ(maj, min) \
-	((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
-# else
-#  define __GNUC_PREREQ(maj, min) 0
-# endif
-#endif
 
-#if defined __x86_64__ && (__LP64__ || !__GNUC_PREREQ (3, 3))
+#if defined __x86_64__ \
+    && (defined __LP64__ || !defined __GNUC__ || __GNUC__ < 4)
 # define __WORDSIZE	64
 # define __WORDSIZE_COMPAT32	1
 #else
