6ec8b75453bad28f643bd355fa3a88e9c2a0bcbd
H.J. Lu <hjl.tools@gmail.com>
2012-03-19 17:51:53 -0700

    Use __builtin_bswap[32|64]  if GCC >= 4.2
    2012-03-19  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* sysdeps/i386/bits/byteswap.h: Include <features.h>.
    	(__bswap_32): Use __builtin_bswap32 if GCC >= 4.2.
    	(__bswap_64): Use __builtin_bswap64 if GCC >= 4.2.
    
    	* sysdeps/x86_64/bits/byteswap.h: Include <features.h>.
    	(__bswap_32): Use __builtin_bswap32 if GCC >= 4.2.
    	(__bswap_64): Use __builtin_bswap64 if GCC >= 4.2.

--- sysdeps/x86_64/bits/byteswap.h
+++ sysdeps/x86_64/bits/byteswap.h
@@ -24,6 +24,7 @@
 #ifndef _BITS_BYTESWAP_H
 #define _BITS_BYTESWAP_H 1
 
+#include <features.h>
 #include <bits/wordsize.h>
 
 /* Swap bytes in 16 bit value.  */
@@ -56,36 +57,8 @@
      ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) |		      \
       (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
 
-#if defined __GNUC__ && __GNUC__ >= 2
-# if defined __x86_64__ || defined __i486__ || defined __pentium__	      \
-     || defined __pentiumpro__ || defined __pentium4__  || defined __k8__     \
-     || defined __athlon__ || defined __k6__ || defined __nocona__	      \
-     || defined __core2__ || defined __geode__ || defined __amdfam10__
-/* To swap the bytes in a word the i486 processors and up provide the
-   `bswap' opcode.  On i386 we have to use three instructions.  */
-#  define __bswap_32(x) \
-     (__extension__							      \
-      ({ register unsigned int __v, __x = (x);				      \
-	 if (__builtin_constant_p (__x))				      \
-	   __v = __bswap_constant_32 (__x);				      \
-	 else								      \
-	   __asm__ ("bswap %0" : "=r" (__v) : "0" (__x));		      \
-	 __v; }))
-# else
-#  define __bswap_32(x)							      \
-     (__extension__							      \
-      ({ register unsigned int __v, __x = (x);				      \
-	 if (__builtin_constant_p (__x))				      \
-	   __v = __bswap_constant_32 (__x);				      \
-	 else								      \
-	   __asm__ ("rorw $8, %w0;"					      \
-		    "rorl $16, %0;"					      \
-		    "rorw $8, %w0"					      \
-		    : "=r" (__v)					      \
-		    : "0" (__x)						      \
-		    : "cc");						      \
-	 __v; }))
-# endif
+#if __GNUC_PREREQ (4,2)
+# define __bswap_32(x)  __builtin_bswap32 (x)
 #else
 # define __bswap_32(x) \
      (__extension__							      \
@@ -95,7 +68,10 @@
 
 #if defined __GNUC__ && __GNUC__ >= 2
 /* Swap bytes in 64 bit value.  */
-# define __bswap_constant_64(x) \
+# if __GNUC_PREREQ (4,2)
+#  define __bswap_64(x)  __builtin_bswap64 (x)
+# else
+#  define __bswap_constant_64(x) \
      (__extension__ ((((x) & 0xff00000000000000ull) >> 56)		      \
 		     | (((x) & 0x00ff000000000000ull) >> 40)		      \
 		     | (((x) & 0x0000ff0000000000ull) >> 24)		      \
@@ -105,22 +81,17 @@
 		     | (((x) & 0x000000000000ff00ull) << 40)		      \
 		     | (((x) & 0x00000000000000ffull) << 56)))
 
-# ifdef __x86_64__ 
 #  if __WORDSIZE == 64
-#   define __bswap_64_uint64	unsigned long int
-#  else
-#   define __bswap_64_uint64	unsigned long long int
-#  endif
-#  define __bswap_64(x) \
+#   define __bswap_64(x) \
      (__extension__							      \
-      ({ register __bswap_64_uint64 __v, __x = (x);			      \
+      ({ register unsigned long __v, __x = (x);				      \
 	 if (__builtin_constant_p (__x))				      \
 	   __v = __bswap_constant_64 (__x);				      \
 	 else								      \
 	   __asm__ ("bswap %q0" : "=r" (__v) : "0" (__x));		      \
 	 __v; }))
-# else
-#  define __bswap_64(x) \
+#  else
+#   define __bswap_64(x) \
      (__extension__                                                           \
       ({ union { __extension__ unsigned long long int __ll;                   \
 		 unsigned int __l[2]; } __w, __r;                             \
@@ -133,6 +104,7 @@
 	     __r.__l[1] = __bswap_32 (__w.__l[0]);                            \
 	   }                                                                  \
 	 __r.__ll; }))
+#  endif
 # endif
 #endif
 
