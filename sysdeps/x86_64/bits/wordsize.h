76e02229ae4b3de15bcca266ece31de491597d56
H.J. Lu <hjl.tools@gmail.com>
2012-04-13 14:51:09 -0700

    Check __x86_64__ and __ILP32__ for __WORDSIZE

--- sysdeps/x86_64/bits/wordsize.h
+++ sysdeps/x86_64/bits/wordsize.h
@@ -1,22 +1,8 @@
-/* Determine the wordsize from the preprocessor defines.  __LP64__ is
-   defined for 64-bit since GCC 3.3.  For compilers other than GCC 3.3
-   or newer, we check if LONG_MAX > INT_MAX.  */
+/* Determine the wordsize from the preprocessor defines.  */
 
-#include <features.h>
-
-#if __GNUC_PREREQ (3, 3)
-# if defined __x86_64__  && defined __LP64__
-#  define __WORDSIZE	64
-#  define __WORDSIZE_COMPAT32	1
-# else
-#  define __WORDSIZE	32
-# endif
+#if defined __x86_64__ && !defined __ILP32__
+# define __WORDSIZE	64
+# define __WORDSIZE_COMPAT32	1
 #else
-#include <limits.h>
-# if defined __x86_64__ && LONG_MAX > INT_MAX
-#  define __WORDSIZE	64
-#  define __WORDSIZE_COMPAT32	1
-# else
-#  define __WORDSIZE	32
-# endif
+# define __WORDSIZE	32
 #endif
