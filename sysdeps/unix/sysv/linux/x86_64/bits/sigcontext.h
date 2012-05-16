da938a15e2146388908a2cc77c4e47ae0008ea4a
H.J. Lu <hjl.tools@gmail.com>
2012-05-16 12:00:40 -0700

    Replace __extension__ unsigned long long int with __uint64_t

--- sysdeps/unix/sysv/linux/x86_64/bits/sigcontext.h
+++ sysdeps/unix/sysv/linux/x86_64/bits/sigcontext.h
@@ -121,37 +121,37 @@ struct _fpstate
 
 struct sigcontext
 {
-  __extension__ unsigned long long r8;
-  __extension__ unsigned long long r9;
-  __extension__ unsigned long long r10;
-  __extension__ unsigned long long r11;
-  __extension__ unsigned long long r12;
-  __extension__ unsigned long long r13;
-  __extension__ unsigned long long r14;
-  __extension__ unsigned long long r15;
-  __extension__ unsigned long long rdi;
-  __extension__ unsigned long long rsi;
-  __extension__ unsigned long long rbp;
-  __extension__ unsigned long long rbx;
-  __extension__ unsigned long long rdx;
-  __extension__ unsigned long long rax;
-  __extension__ unsigned long long rcx;
-  __extension__ unsigned long long rsp;
-  __extension__ unsigned long long rip;
-  __extension__ unsigned long long eflags;
+  __uint64_t r8;
+  __uint64_t r9;
+  __uint64_t r10;
+  __uint64_t r11;
+  __uint64_t r12;
+  __uint64_t r13;
+  __uint64_t r14;
+  __uint64_t r15;
+  __uint64_t rdi;
+  __uint64_t rsi;
+  __uint64_t rbp;
+  __uint64_t rbx;
+  __uint64_t rdx;
+  __uint64_t rax;
+  __uint64_t rcx;
+  __uint64_t rsp;
+  __uint64_t rip;
+  __uint64_t eflags;
   unsigned short cs;
   unsigned short gs;
   unsigned short fs;
   unsigned short __pad0;
-  __extension__ unsigned long long err;
-  __extension__ unsigned long long trapno;
-  __extension__ unsigned long long oldmask;
-  __extension__ unsigned long long cr2;
+  __uint64_t err;
+  __uint64_t trapno;
+  __uint64_t oldmask;
+  __uint64_t cr2;
   struct _fpstate * fpstate;
 # ifdef __ILP32__
   unsigned int pad0;
 # endif
-  __extension__ unsigned long long __reserved1 [8];
+  __uint64_t __reserved1 [8];
 };
 
 #endif /* __x86_64__ */
