4e5b77d12c06eed7106de9550ea0d59beb0e2f5b
H.J. Lu <hjl.tools@gmail.com>
2012-05-19 09:29:47 -0700

    Rename LOAD_ARG2_TYPE to LOAD_ARG4_TYPE in x32 posix_fadvise.c

--- sysdeps/unix/sysv/linux/x86_64/x32/posix_fallocate.c
+++ sysdeps/unix/sysv/linux/x86_64/x32/posix_fallocate.c
@@ -1,8 +1,15 @@
 #include <sysdep.h>
 
-#undef LOAD_ARG2_TYPE
-#define LOAD_ARG2_TYPE long long int
+/* Since posix_fallocate is implemented by calling the fallocate
+   system call with: fallocate (fd, 0, offset, len), we redefine
+   arguments 3 (offset) and 4 (len) to be long long int to match:
+
+   int fallocate(int fd, int mode, off_t offset, off_t len);
+ */
+
 #undef LOAD_ARG3_TYPE
 #define LOAD_ARG3_TYPE long long int
+#undef LOAD_ARG4_TYPE
+#define LOAD_ARG4_TYPE long long int
 
 #include <sysdeps/unix/sysv/linux/wordsize-64/posix_fallocate.c>
