79d93ba35b473cf363bae80531a3e6f94be9222e
H.J. Lu <hjl.tools@gmail.com>
2012-05-22 13:36:34 -0700

    Remove conditionals and add some comments

--- sysdeps/unix/sysv/linux/sysctl.mk
+++ sysdeps/unix/sysv/linux/sysctl.mk
@@ -1,3 +1,3 @@
-ifeq ($(subdir),misc)
+# sysctl system call has been deprecated.  It is provided for backward
+# compatility.  New target should override it (see x86_64/x32/sysctl.mk).
 sysctl = sysctl
-endif
--- sysdeps/unix/sysv/linux/x86_64/x32/sysctl.mk
+++ sysdeps/unix/sysv/linux/x86_64/x32/sysctl.mk
@@ -1,4 +1,2 @@
 # X32 doesn't support sysctl.
-ifeq ($(subdir),misc)
 sysctl =
-endif
