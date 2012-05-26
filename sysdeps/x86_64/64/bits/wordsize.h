1e4edc3926537cf555e4dbc4470e0b73f23dd244
H.J. Lu <hjl.tools@gmail.com>
2012-05-26 11:37:53 -0700

    Check __ILP32__ instead of __LP64__ for __WORDSIZE

--- sysdeps/x86_64/64/bits/wordsize.h
+++ sysdeps/x86_64/64/bits/wordsize.h
@@ -1,9 +1,6 @@
-/* Determine the wordsize from the preprocessor defines.  __LP64__ is
-   defined for 64bit since GCC 3.3.  For GCC older than GCC 4.0, if
-   __x86_64__ is defined, it must be 64bit.  */
+/* Determine the wordsize from the preprocessor defines.  */
 
-#if defined __x86_64__ \
-    && (defined __LP64__ || !defined __GNUC__ || __GNUC__ < 4)
+#if defined __x86_64__ && !defined __ILP32__
 # define __WORDSIZE	64
 # define __WORDSIZE_COMPAT32	1
 #else
