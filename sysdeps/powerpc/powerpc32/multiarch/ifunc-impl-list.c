06180076cbf5ebc380af734a599b5226f2ecb27f
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2013-04-18 14:07:54 -0500

    PowerPC: strnlen multilib for PowerPC32

--- sysdeps/powerpc/powerpc32/multiarch/ifunc-impl-list.c
+++ sysdeps/powerpc/powerpc32/multiarch/ifunc-impl-list.c
@@ -56,6 +56,13 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
 			      __bzero_power4)
 	      IFUNC_IMPL_ADD (array, i, bzero, 1, __bzero_ppc32))
 
+  /* Support sysdeps/powerpc/powerpc32/multiarch/strnlen.c.  */
+  IFUNC_IMPL (i, name, strnlen,
+	      IFUNC_IMPL_ADD (array, i, strnlen, hwcap & PPC_FEATURE_HAS_VSX,
+			      __strnlen_power7)                                 
+	      IFUNC_IMPL_ADD (array, i, strnlen, 1,
+			      __strnlen_ppc32)) 
+
 #ifdef SHARED
   /* Support sysdeps/powerpc/powerpc32/multiarch/memset.c.  */
   IFUNC_IMPL (i, name, memset,
