f61e1a4420747d9fca4f867eeb6ca7a4143f87a0
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2013-04-19 07:32:33 -0500

    PowerPC: memchr multilib for PowerPC32

--- sysdeps/powerpc/powerpc32/multiarch/ifunc-impl-list.c
+++ sysdeps/powerpc/powerpc32/multiarch/ifunc-impl-list.c
@@ -86,6 +86,14 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
 	      IFUNC_IMPL_ADD (array, i, mempcpy, 1,
 			      __mempcpy_ppc32))
 
+  /* Support sysdeps/powerpc/powerpc32/multiarch/memchr.c.  */
+  IFUNC_IMPL (i, name, memchr,
+	      IFUNC_IMPL_ADD (array, i, memchr,
+			      hwcap & PPC_FEATURE_HAS_VSX,
+			      __memchr_power7)
+	      IFUNC_IMPL_ADD (array, i, memchr, 1,
+			      __memchr_ppc32))
+
 #ifdef SHARED
   /* Support sysdeps/powerpc/powerpc32/multiarch/memset.c.  */
   IFUNC_IMPL (i, name, memset,
