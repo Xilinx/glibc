39a80697fb345910567c7fdea9e7bf7c12709eac
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2013-04-19 14:10:32 -0500

    PowerPC: strncasecmp multilib for PowerPC32

--- sysdeps/powerpc/powerpc32/multiarch/ifunc-impl-list.c
+++ sysdeps/powerpc/powerpc32/multiarch/ifunc-impl-list.c
@@ -78,6 +78,21 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
 	      IFUNC_IMPL_ADD (array, i, strcasecmp_l, 1,
 			      __strcasecmp_l_ppc32))
 
+  /* Support sysdeps/powerpc/powerpc32/multiarch/strncase.c.  */
+  IFUNC_IMPL (i, name, strncasecmp,
+	      IFUNC_IMPL_ADD (array, i, strncasecmp,
+			      hwcap & PPC_FEATURE_HAS_VSX,
+			      __strncasecmp_power7)
+	      IFUNC_IMPL_ADD (array, i, strncasecmp, 1, __strncasecmp_ppc32))
+
+  /* Support sysdeps/powerpc/powerpc32/multiarch/strncase_l.c.  */
+  IFUNC_IMPL (i, name, strncasecmp_l,
+	      IFUNC_IMPL_ADD (array, i, strncasecmp_l,
+			      hwcap & PPC_FEATURE_HAS_VSX,
+			      __strncasecmp_l_power7)
+	      IFUNC_IMPL_ADD (array, i, strncasecmp_l, 1,
+			      __strncasecmp_l_ppc32))
+
   /* Support sysdeps/powerpc/powerpc32/multiarch/mempcpy.c.  */
   IFUNC_IMPL (i, name, mempcpy,
 	      IFUNC_IMPL_ADD (array, i, mempcpy,
---
+++ sysdeps/powerpc/powerpc32/multiarch/strncase-power7.c
@@ -0,0 +1,4 @@
+#include <string.h>
+
+#define __strncasecmp __strncasecmp_power7
+#include <string/strncase.c>
