f440a5b7c28e061c2305e9476988031e407c8051
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2013-04-22 12:12:55 -0500

    PowerPC: llround/llroundf cleanup

--- sysdeps/powerpc/powerpc32/fpu/multiarch/s_llround-c.c
+++ sysdeps/powerpc/powerpc32/fpu/multiarch/s_llround-c.c
@@ -1,3 +1,32 @@
+/* llround function.  PowerPC32 default version.
+   Copyright (C) 2004-2013 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <math.h>
+#include <math_ldbl_opt.h>
+
 #undef __llround
 #define __llround __llround_ppc32
+
+#undef weak_alias
+#define weak_alias(a,b)
+#undef strong_alias
+#define strong_alias(a,b)
+#undef compat_symbol
+#define compat_symbol(a,b,c,d)
+
 #include <sysdeps/powerpc/fpu/s_llround.c>
--- sysdeps/powerpc/powerpc32/fpu/multiarch/s_llround.c
+++ sysdeps/powerpc/powerpc32/fpu/multiarch/s_llround.c
@@ -1,4 +1,4 @@
-/* Multiple versions of s_llround.
+/* Multiple versions of llround.
    Copyright (C) 2013 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
