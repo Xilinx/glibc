f8f39c7a379b548eaafa06f37595b7a60def9758
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2013-06-05 07:51:58 -0500

    PowerPC: mempcpy multilib for PowerPC32

--- sysdeps/powerpc/powerpc32/multiarch/ifunc-impl-list.c
+++ sysdeps/powerpc/powerpc32/multiarch/ifunc-impl-list.c
@@ -88,6 +88,15 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
 	      IFUNC_IMPL_ADD (array, i, memcmp, hwcap & PPC_FEATURE_POWER4,
 			      __memcmp_power4)
 	      IFUNC_IMPL_ADD (array, i, memcmp, 1, __memcmp_ppc32))
+
+  /* Support sysdeps/powerpc/powerpc32/multiarch/mempcpy.c.  */
+  IFUNC_IMPL (i, name, mempcpy,
+	      IFUNC_IMPL_ADD (array, i, mempcpy,
+			      hwcap & PPC_FEATURE_HAS_VSX,
+			      __mempcpy_power7)
+	      IFUNC_IMPL_ADD (array, i, mempcpy, 1,
+			      __mempcpy_ppc32))
+
 #endif
 
   return i;
